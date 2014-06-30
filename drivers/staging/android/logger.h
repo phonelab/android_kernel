5ed28aed3b1f43a7d11ccd05459ff0a611c832e6
Jinghao Shi <jinghaos@buffalo.edu>
2014-06-30 15:00:10 -0400

    enlarge max log line length, and log buffer

--- drivers/staging/android/logger.h
+++ drivers/staging/android/logger.h
@@ -56,7 +56,7 @@ struct logger_entry {
 #define LOGGER_LOG_SYSTEM	"log_system"	/* system/framework messages */
 #define LOGGER_LOG_MAIN		"log_main"	/* everything else */
 
-#define LOGGER_ENTRY_MAX_PAYLOAD	4076
+#define LOGGER_ENTRY_MAX_PAYLOAD	65516
 
 #define __LOGGERIO	0xAE
 
